0c54e9b10770ed3ea7ba4b8c8b0c35282b6617f8
Saad Malik <universitysemester485@gmail.com>
2026-09-03 11:27:57 +0500

    mms

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -10,7 +10,7 @@ function getAdminApp() {
   const projectId = process.env.FIREBASE_PROJECT_ID;
   const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
   let privateKey = process.env.FIREBASE_PRIVATE_KEY;
-
+console.log("DEBUG Admin SDK projectId:", process.env.FIREBASE_PROJECT_ID);
 
   if (privateKey) {
    
